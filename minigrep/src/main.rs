61370e0f7bfc6740ed84171a6817d1a961f7e635
joao-gfs <joaoguilhermeferreira19@gmail.com>
2025-01-16 20:40:01 -0300

    ler args e abrir file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,16 @@
+/* Clone simplficado do comando grep existente em Unix */
+
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+    
+    println!("In file {file_path}");
+
+    let content = fs::read_to_string(file_path).expect("Cannot open the file");
+
+    println!("With text:\n{content}");
 }
